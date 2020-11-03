a95852b14037e3ba179c89a5c102505b1c647fde
Mihail <pedrael@pop-os.localdomain>
2020-11-03 23:00:13 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -45,5 +45,56 @@ function countDown() {
   }, 1000)
 }
 
+function animateLoading() {
+  let l_progress = document.querySelector('#loading-progress')
+  let l_value = document.querySelector('#loading-value')
+  if(!l_progress || !l_value) return
+  let l_width = l_progress.offsetWidth
+  let i = 1
+  let timerId = setInterval(() => {
+    l_value.innerHTML ='загрузка: ' + i + '%'
+    l_progress.style.left = -100 + i*0.9 + '%'
+    i++
+    if(i == 99) clearInterval(timerId)
+  }, 6)
+  // for(let i = 1; i < 99; i++) {
+  //   l_value.innerHTML ='загрузка: ' + i + '%'
+  //   l_progress.style.left = -100 + i*0.9 + '%'
+  // }
+}
+
+function togglePopup() {
+  let open = document.querySelectorAll('.js-open-popup')
+  let popup = document.querySelector('.popup-message')
+  let close = popup.querySelector('.js-close-popup')
+  if(!open || !popup || !close) return
+  if(open != null) {
+    open.forEach((i) => {
+      i.addEventListener('click', (e) => {
+        e.preventDefault()
+        popup.style.display = 'flex'
+      })
+    })
+  }
+
+  if(close != null) {
+    close.addEventListener('click', () => {
+      popup.style.display = 'none'
+    })
+  }
+
+  // let switchItem = popup.querySelector('.switch')
+  // if(switchItem != null) switchItem.addEventListener('click', () => {
+  //   let switches = switchItem.querySelectorAll('.switch-btn')
+  //   switches.forEach((sw) => {
+  //     sw.classList.toggle('switched')
+  //   })
+  //   popup.querySelector('.social-if').classList.toggle('disabled')
+  //   popup.querySelector('.contact-if').classList.toggle('disabled')
+  // })
+}
+
 todayDate()
 countDown()
+animateLoading()
+togglePopup()
